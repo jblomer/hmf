90e8aa545a15d906d24ae76d1010e1d5795c3734
Jakob Blomer <jblomer@cern.ch>
2015-10-09 16:56:24 +0200

    more meat

--- mpi/mmpi.cc
+++ mpi/mmpi.cc
@@ -1,9 +1,15 @@
 
+#include <mpi.h>
+#include <pthread.h>
+
 #include <cassert>
 #include <cstdio>
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <vector>
+
+#include "murmur.h"
 
 #include "Riostream.h"
 #include "TChain.h"
@@ -17,6 +23,8 @@
 
 using namespace std;
 
+int world_rank, world_size;
+
 Int_t total_histos;
 Int_t total_bins;
 Int_t defined_bins;
@@ -26,6 +34,15 @@ Int_t defined_bins_sparse;
 
 unordered_map<int, string> hist_names;
 unordered_map<int, unordered_map<int, double> > hist;
+
+struct Bin {
+  Bin() : nhist(0), nbin(0), val(.0), err(.0) { }
+  Bin(int n, int b, double v) : nhist(n), nbin(b), val(v), err(.0) { }
+  int nhist;
+  int nbin;
+  double val;
+  double err;
+};
 
 static void Usage(const char *progname) {
   printf("usage: mpirun %s <ROOT histograms>\n", progname);
@@ -162,12 +179,84 @@ static void ReadHistos(char *input_path) {
   cerr << "Defined sparse bins: " << defined_bins_sparse << endl;
 }
 
+static void *main_recv(void *data) {
+  for (int i = 0; i < world_size; ++i) {
+    unsigned size;
+    MPI_Recv(&size, sizeof(size), MPI_BYTE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+    Bin *recv_buf = (Bin *)malloc(size * sizeof(Bin));
+    MPI_Recv(recv_buf, size * sizeof(Bin), MPI_BYTE, MPI_ANY_SOURCE, 0,
+             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    printf("  I'm %d, got %dth data packet\n", world_rank, i);
+    for (unsigned i = 0; i < size; ++i) {
+      hist[recv_buf[i].nhist][recv_buf[i].nbin] += recv_buf[i].val;
+    }
+    free(recv_buf);
+  }
+  return NULL;
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     Usage(argv[0]);
     return 1;
   }
+
+  int claimed, provided;
+  MPI_Init_thread(0, 0, MPI_THREAD_MULTIPLE, &provided);
+  MPI_Query_thread(&claimed);
+  printf("Query thread level= %d  Init_thread level= %d\n", claimed, provided);
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
   ReadHistos(argv[1]);
 
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  printf("rank %d, filling dispatch vector\n", world_rank);
+
+  vector<vector<Bin>> dispatch;
+  for (int i = 0; i < world_size; ++i) {
+    dispatch.push_back(vector<Bin>());
+  }
+
+  for (auto i = hist.begin(); i != hist.end(); ++i) {
+    for (auto j = i->second.begin(); j != i->second.end(); ++j) {
+      struct {
+        int a;
+        int b;
+      } global_bin;
+      global_bin.a = i->first;
+      global_bin.b = j->second;
+      uint32_t hashed = MurmurHash2(&global_bin, sizeof(global_bin), 42);
+      dispatch[hashed % world_rank].push_back(
+        Bin(i->first, j->first, j->second));
+    }
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  printf("rank %d, sending and receiving data\n", world_rank);
+
+  pthread_t thread_recv;
+  pthread_create(&thread_recv, NULL, main_recv, NULL);
+  for (int i = 0; i < world_size; ++i) {
+    printf("I'm rank %d, sending data to rank %d\n", world_rank, i);
+    unsigned size = dispatch[i].size();
+    MPI_Ssend(&size, sizeof(unsigned), MPI_BYTE, i, 0, MPI_COMM_WORLD);
+    MPI_Ssend(&dispatch[i][0], dispatch[i].size() * sizeof(Bin), MPI_BYTE,
+              i, 0, MPI_COMM_WORLD);
+  }
+  pthread_join(thread_recv, NULL);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  if (world_rank != 0) {
+    MPI_Finalize();
+    return 0;
+  }
+
+  printf("Here's your master thread, reading out results.\n");
+  MPI_Finalize();
   return 0;
 }
